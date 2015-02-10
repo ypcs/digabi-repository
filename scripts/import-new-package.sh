cbb66bd31537888bab394d312f5e2fa4db8256e8
Ville Korhonen <ville@xd.fi>
2015-02-10 19:04:16 +0200

    Add new TODOs.

---
+++ scripts/import-new-package.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+. $(dirname $0)/common.sh
+
+# Import new package to *unstable*
+
+# TODO: Support importing packages from mirrors
+# TODO: Support importing packages from manual builds
+# TODO: Support incoming -style folders
+# TODO: Track from which origin package is from, and do not allow it to migrate from another sources
