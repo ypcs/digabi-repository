bfb7ad09f6865f60dab5cd8adf69fb0106536c43
Ville Korhonen <ville@xd.fi>
2015-02-23 13:29:34 +0200

    Do not list repositories.

--- scripts/import-new-package.sh
+++ scripts/import-new-package.sh
@@ -13,8 +13,6 @@ set -e
 REPO="$1"
 shift
 PACKAGES="$@"
-echo "D: List repositories..."
-${APTLYCMD} repo list -raw
 
 echo "I: Add package(s) to repository ${REPO}..."
 ${APTLYCMD} repo add ${REPO} ${PACKAGES}
